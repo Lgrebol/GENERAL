df2e43e2848700271120375a6dda78ee541b9896
Lgrebol <lgrebol08@boscdelacoma.cat>
2024-10-25 18:19:24 +0200

    Test correctly passed and functionality also (should fetch cocktails from the API)

--- API/src/app/services/cocktail.service.spec.ts
+++ API/src/app/services/cocktail.service.spec.ts
@@ -1,4 +1,3 @@
-// src/app/services/cocktail.service.spec.ts
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { CocktailService } from './cocktail.service';
--- API/src/app/services/cocktail.service.ts
+++ API/src/app/services/cocktail.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface Cocktail {
+  idDrink: string;
+  strDrink: string;
+  strDrinkThumb: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CocktailService {
+  private apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getCocktails(): Observable<Cocktail[]> {
+    return this.http.get<Cocktail[]>(this.apiUrl);
+  }
 }
