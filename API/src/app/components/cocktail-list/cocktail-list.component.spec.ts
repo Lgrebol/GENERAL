e30c9011ab8aa71082acf35006e6a7ef2d14afb7
Lgrebol <lgrebol08@boscdelacoma.cat>
2024-10-25 18:21:32 +0200

    Test it should display a list of cocktails done

--- API/src/app/components/cocktail-list/cocktail-list.component.spec.ts
+++ API/src/app/components/cocktail-list/cocktail-list.component.spec.ts
@@ -1,23 +1,32 @@
+// src/app/components/cocktail-list/cocktail-list.component.spec.ts
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CocktailListComponent } from './cocktail-list.component';
+import { CocktailService } from '../../services/cocktail.service';
+import { of } from 'rxjs';
 
 describe('CocktailListComponent', () => {
   let component: CocktailListComponent;
   let fixture: ComponentFixture<CocktailListComponent>;
+  let cocktailService: CocktailService;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [CocktailListComponent]
-    })
-    .compileComponents();
+  beforeEach(() => {
+    const cocktailServiceMock = {
+      getCocktails: () => of([{ idDrink: '11007', strDrink: 'Margarita', strDrinkThumb: 'image_url' }])
+    };
+
+    TestBed.configureTestingModule({
+      declarations: [CocktailListComponent],
+      providers: [{ provide: CocktailService, useValue: cocktailServiceMock }]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CocktailListComponent);
     component = fixture.componentInstance;
+    cocktailService = TestBed.inject(CocktailService);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should display a list of cocktails', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.cocktail-name').textContent).toContain('Margarita');
   });
 });
